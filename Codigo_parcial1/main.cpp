8dcd5fbdff504df36b3d5e4de4f37ef9767621ad
teomesa <mateo.restrepom@udea.edu.co>
2020-10-17 16:58:19 -0500

    Segundo commit

--- Codigo_parcial1/main.cpp
+++ Codigo_parcial1/main.cpp
@@ -8,8 +8,9 @@ void formulas(int [], int [], int, int);
 int main()
 {
     //Coordenadas de los cañones
-    int coordenadasO[2]={0,0}, coordenadasD[2];
-    cout << "Las coordenadas iniciales del canon O son (0,0)" << endl;
+    int coordenadasO[2]={0}, coordenadasD[2];
+    cout << "Ingrese la posicion en Y del canonO";
+    cin >> coordenadasO[1];
     cout << "Ingrese las coordenadas del canon D (x,y): ";
     for (int i=0;i<2;i++){
         cin >> coordenadasD[i];
@@ -24,12 +25,31 @@ int main()
     float VxO,VyO;
     VxO=velocidadO*sin(anguloO);
     VyO=velocidadO*cos(anguloO);
-    cout << VxO << endl;
-    cout << VyO << endl;
+    int caso;
+    cout << "ingrese el caso quiere ver: ";
+    cout << "Generar disparos (al menos tres) ofensivos que comprometan la integridad del cañón defensivo" << endl;
+    cout << "Generar disparos (al menos tres) defensivos que comprometan la integridad del cañón ofensivo" << endl;
+    cout << "Dado un disparo ofensivo, generar (al menos tres) disparos defensivos que impida que el canón defensivo sea destruido sin importar si el cañón ofensivo pueda ser destruido." << endl;
+    cout << "Dado un disparo ofensivo, generar (al emnos tres) disparo defensivos que impidan que los canones defensivo y ofensivo puedan ser destruidos." << endl;
+    cin >> caso;
+    switch (caso) {
+    case 1:
+    {
+        float Rd=0.025;
+    }
+    }
 }
 
-void formulas(int posicionO[], int posicionD[], int velocidadx, int velocidady){
-    int x=posicionO[0]+velocidadx;
+void formulas(int posicionO[2], int velocidadx, int velocidady){
+    int t=0;
+    int t1=(-velocidady+sqrt(pow(velocidady,2)-4*posicionO[1]*-4.90))/2*posicionO[0];
+    int t2=(-velocidady-sqrt(pow(velocidady,2)-4*posicionO[1]*-4.90))/2*posicionO[0];
+    if (t1>=0)
+        t=t1;
+    else
+        t=t2;
+    int x=posicionO[0]+velocidadx*t;
     int Vx=velocidadx;
-    int y=posicionO[1]+velocidady;
+    int y=posicionO[1]+velocidady*t-(9.81/2)*pow(t, 2);
+    int Vy=-9.8*t;
 }
